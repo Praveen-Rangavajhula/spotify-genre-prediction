6bc66cb884d0b4c6119a822a7b1f40bc2b07ed15
Praveen-Rangavajhula <149396088+Praveen-Rangavajhula@users.noreply.github.com>
2024-11-13 18:44:07 -0500

    Added model files and profile report

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,5 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 def load_spotify_dataset():
-    pass
+    # Load the dataset
+    df_spotify = pd.read_csv('../data/raw/dataset.csv', index_col=0)
+
 
 def standardize_data():
     pass
