bbb43424a59ab1b65e56a34692e393b2007b4f42
Praveen-Rangavajhula <149396088+Praveen-Rangavajhula@users.noreply.github.com>
2024-11-13 20:27:03 -0500

    Perform EDA and Preprocess Spotify Dataset for Model Training (#1)
    * testing changes
    
    * Updated EDA analysis and created data_loader.py
    
    * Added model files and profile report
    
    * Created data_loader.py methods which preprocess and split the data
    
    ---------
    
    Co-authored-by: Eleanor Zhu <124330947+Eleanor-Zhu@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import data.data_loader
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from data.data_loader import load_spotify_dataset
+
+def main():
+    # Step 1: Load and preprocess data
+    print("Loading and preprocessing data...")
+    df_spotify, target_column = load_spotify_dataset()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
